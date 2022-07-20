f01f6e2496c1ac732383ac29d91dc847cfb8f97e
shemnduati <nduatishem@gmail.com>
2022-07-20 18:52:08 +0300

    done with the dashboard

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -35,8 +35,7 @@
                 <div class="sidebar-brand-icon rotate-n-15">
                     <i class="fas fa-laugh-wink"></i>
                 </div>
-                <div class="sidebar-brand-text mx-3">Football</div>
-                Predicitons
+                <div class="sidebar-brand-text mx-3">Football-Pre</div>
             </a>
 
             <!-- Divider -->
